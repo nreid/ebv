6c36f34b528e8dda2286dbf845a36aea8dddd898
Noah Reid <noah.reid@uconn.edu>
2020-10-23 14:55:55 -0400

    combine

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ebv
 
 
+original data located here
+`/labs/Oneill/jules`
+
+new project directory here:
+`/labs/CBC/core_projects/2020/ebv/`
+
--- scripts/02_combine_lanes.sh
+++ scripts/02_combine_lanes.sh
@@ -21,10 +21,53 @@ TOTAL=../data/total_fastq
 
 # create output directories
 COMBS=../data/small_combined
+if [ -d $COMBS ]; then rm -r $COMBS; fi
 mkdir -p $COMBS
 
 COMBT=../data/total_combined
+if [ -d $COMBT ]; then rm -r $COMBT; fi
 mkdir -p $COMBT
+
+METAPE=../meta/fastq_pe.txt
+METASE=../meta/fastq_se.txt
+
+
+
+# combine fastq files for total RNA libraries, renaming using namefunPE function
+namefunPE () {
+	OUT=${3}_${4}_${5}_${7}.fastq.gz
+	echo $OUT
+}
+
+for i in {1..96}
+do 
+	F=$(sort -k 6,6 $METAPE | sed -n ${i}p | cut -f 1)
+
+	OUT=$(namefunPE $(sort -k 6,6 $METAPE | sed -n ${i}p))
+
+	echo $OUT $F
+	
+	cat $TOTAL/$F >>$COMBT/$OUT
+
+done
+
+# combine fastq files for small RNA libraries, renaming using namefunSE function
+namefunSE () {
+	OUT=${2}_${3}_${4}.fastq.gz
+	echo $OUT
+}
+
+for i in {1..116}
+do 
+	F=$(sort -k 5,5 $METASE | sed -n ${i}p | cut -f 1)
+
+	OUT=$(namefunSE $(sort -k 5,5 $METASE | sed -n ${i}p))
+
+	echo $OUT $F
+	
+	cat $SMALL/$F >>$COMBS/$OUT
+
+done
 
 
 
