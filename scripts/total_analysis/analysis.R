ea8247f8bdc9583d351a953ccc70e60ea2988b84
Noah Reid <noah.reid@uconn.edu>
2020-11-02 12:38:30 -0500

    analysis

--- scripts/total_analysis/analysis.R
+++ scripts/total_analysis/analysis.R
@@ -110,6 +110,18 @@ colSums(m[prevalence<2,])/colSums(m)
 # what is the distribution of total expression across samples?
 rowSums(m) %>% log(.,10) %>% hist(.,100)
 
+subg <- rowSums(m) > 20
+table(subg)
+
+m <- m[subg,]
+mylength <- mylength[subg]
+
+# downstream analysis shows that miA_1_S5 is a very problematic sample. exclude it.
+	# in the PCA, it is way off by itself on PC1, which explains 43% of the variance of the rld transformed counts
+
+m <- m[,!(colnames(m) %in% "miA_1_S5")]
+myfactors <- myfactors[myfactors[,1]!="miA_1_S5",]
+
 #################################################
 #  Analyze the data using DESeq2
 #################################################
@@ -141,4 +153,117 @@ ddsHTSeq$CellLine <- factor(ddsHTSeq$CellLine, levels = treatments)
 # verify the order
 ddsHTSeq$CellLine
 
+######################################################
+# Run the statistical analysis
+######################################################
+
+dds <- DESeq(ddsHTSeq)
+
+######################################################
+# Data visualization
+######################################################
+
+# MA plot
+plotMA(res_shrink, ylim=c(-4,4))
+
+# distribution of log2 fold changes:
+  # there should be a peak at 0
+hist(resDE$log2FoldChange,breaks=200)
+abline(v=0,col="red",lwd=2)
+
+##############
+
+#Volcano plot
+
+# negative log-scaled adjusted p-values
+log_padj <- -log(res_shrink$padj,10)
+log_padj[log_padj > 100] <- 100
+
+# plot
+plot(x=res_shrink$log2FoldChange,
+     y=log_padj,
+     pch=20,
+     cex=.2,
+     col=(log_padj > 3)+1, # color padj < 0.1 red
+     ylab="negative log-scaled adjusted p-value",
+     xlab="shrunken log2 fold changes")
+
+#############
+
+# PCA plot
+
+# normalized, variance-stabilized transformed counts for visualization
+vsd <- vst(dds, blind=FALSE)
+
+plotPCA(vsd, intgroup="CellLine")
+
+# alternatively, using ggplot
+
+dat <- plotPCA(vsd, intgroup="CellLine",returnData=TRUE)
+
+p <- ggplot(dat,aes(x=PC1,y=PC2,col=group))
+p <- p + geom_point() + geom_text_repel(aes(label=name))
+p
+
+
+
+######################################################
+# Get a table of results
+######################################################
+
+# list coefficient names
+
+resultsNames(dds)
+
+# get results table
+# resDE <- results(dds, name="CellLine_sh_vs_RP")
+resDE <- results(dds, contrast=c("CellLine","RE","mi"))
+
+# get a quick summary of the table
+summary(resDE)
+
+
+######################################################
+# Get a table of shrunken log2 fold changes
+######################################################
+
+# see coefficient names:
+resultsNames(dds)
+
+# get shrunken log fold changes, specifying the coefficient 
+# res_shrink <- lfcShrink(dds, coef="CellLine_sh_vs_RP", type="ashr")
+res_shrink <- lfcShrink(dds, contrast=c("CellLine","RE","mi"),type="ashr")
+	
+
+
+
+
+##############
+
+# heatmap of DE genes
+
+# regularized log transformation of counts
+rld <- rlog(dds, blind=FALSE)
+
+# get top 50 log fold change genes (excluding cook's cutoff outliers)
+top50 <- data.frame(res_shrink) %>%
+  filter(!is.na(padj)) %>% 
+  arrange(-abs(log2FoldChange)) %>% 
+  rownames() %>% 
+  head(.,n=50)
+
+df <- data.frame(colData(dds)[,"CellLine"])
+  rownames(df) <- colnames(dds)
+  colnames(df) <- "CellLine"
+
+pheatmap(
+  assay(rld)[top50,], 
+  cluster_rows=TRUE, 
+  show_rownames=TRUE,
+  cluster_cols=FALSE,
+  annotation_col=df
+  )
+
+
+plotCounts(dds, gene="ENST00000070846.10", intgroup="CellLine")
 
