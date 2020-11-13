7096330ae1ca1b6d3f54a753ada1c58060df1b0b
Noah Reid <noah.reid@uconn.edu>
2020-11-13 14:05:12 -0500

    bwa

---
+++ scripts/small_analysis/02_bwa_map.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --job-name=bwa_map
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --mem=30G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --array=[0-28]
+##SBATCH --mail-type=ALL
+##SBATCH --mail-user=
+#SBATCH -o %x_%A_%a.out
+#SBATCH -e %x_%A_%a.err
+
+hostname
+date
+
+##########################################
+## quantify expression with kallisto	##	
+##########################################
+
+module load bwa/0.7.17
+module load samtools/1.9
+
+# input, output directories
+INDEX=../../genome/small_transcripts.bwa
+INDIR=../../results/05a_small_trimmed
+OUTDIR=../../results/small_analysis/bwa_trans_counts
+mkdir -p $OUTDIR
+
+# a bash array containing the sample IDs
+LIST=($(ls $INDIR/*trim.fastq.gz | sed 's/.*trimmed\///' | sed 's/.trim.*//'))
+
+# get one sample ID using the slurm array task ID
+SAM=${LIST[$SLURM_ARRAY_TASK_ID]}
+
+bwa mem -k 15 ${SAM}.trim.fastq.gz | \
+samtools view -S -h -u - | \
+samtools sort -T $SAM - >$OUTDIR/${SAM}.bam
+
+date 
+
