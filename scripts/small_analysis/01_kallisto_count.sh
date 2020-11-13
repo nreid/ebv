d43144fbeb273f040a5c1d18d97bb61d7a7e7e50
Noah Reid <noah.reid@uconn.edu>
2020-11-13 13:51:54 -0500

    kallisto

--- scripts/small_analysis/01_kallisto_count.sh
+++ scripts/small_analysis/01_kallisto_count.sh
@@ -33,8 +33,10 @@ LIST=($(ls $INDIR/*trim.fastq.gz | sed 's/.*trimmed\///' | sed 's/.trim.*//'))
 # get one sample ID using the slurm array task ID
 SAM=${LIST[$SLURM_ARRAY_TASK_ID]}
 
+# kallisto requires for single end reads the mean and sd of length distribution
+# get this from 100k reads using bioawk. 
+bioawk=~/bin/bioawk/bioawk
 MEAN=$(zcat $INDIR/${SAM}.trim.fastq.gz | head -n 10000 | $bioawk -c fastx '{n+=length($seq)}END{print n/NR}')
-
 SD=$(zcat $INDIR/${SAM}.trim.fastq.gz | head -n 10000 | $bioawk -v m=$MEAN -c fastx '{n+=(length($seq)-m)^2}END{print sqrt(n/(NR-1))}')
 
 kallisto quant \
